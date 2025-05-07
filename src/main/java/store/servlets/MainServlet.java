59bf8c12fe99e8b8c45629ffdf9ca46219d0122d
lexa19132 <lesha19132@gmail.com>
2025-05-07 17:23:19 +0300

    basic logback configured

--- src/main/java/store/servlets/MainServlet.java
+++ src/main/java/store/servlets/MainServlet.java
@@ -2,6 +2,9 @@ package store.servlets;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,10 +13,12 @@ import jakarta.servlet.http.HttpServletResponse;
 public class MainServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainServlet.class);
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		logger.info("MainServlet accepted the request.");
 		super.doGet(req, resp);
 	}
 
